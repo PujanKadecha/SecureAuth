b5bc5075ed55e07d1826d852ad308a1a9643ec18
PujanKadecha <pujannk@gmail.com>
2026-09-09 12:42:14 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(
-      `Server running and listening on IPv4 at http://localhost:${PORT}`,
+      `Server running on http://localhost:${PORT}`,
     );
   });
 });
